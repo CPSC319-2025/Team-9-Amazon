f52398892e80800a7985f69bafb4cae762759a15
Ke Ren <rkglimmer16@gmail.com>
2025-02-20 15:12:04 -0800

    Revert "Applicant Module code"
    This reverts commit d8a90c2d040dc82feea1fad04845d49e1d91a65c.

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,25 +1,11 @@
-import { useNavigate } from "react-router";
-import CustomButton from "../components/Common/Buttons/CustomButton";
+// import { Link } from "react-router";
 
 const NotFoundPage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="text-center p-8 bg-white rounded-sm shadow-md max-w-2xl">
-        <h1 className="text-6xl font-bold text-[#232F3E] mb-4">404</h1>
-        <h2 className="text-2xl font-medium text-[#232F3E] mb-4">Page Not Found</h2>
-        <p className="text-gray-600 mb-6">
-          The page you're looking for doesn't exist or has been moved.
-        </p>
-        <CustomButton
-          onClick={() => navigate('/')}
-        >
-          Back to Home
-        </CustomButton>
+    return (
+      <div>
+        <h1>404 Not Found</h1>
       </div>
-    </div>
-  );
-};
-
-export default NotFoundPage;
+    );
+  };
+  
+  export default NotFoundPage;
--- frontend/src/routes/ AppRoutes.tsx
+++ frontend/src/routes/ AppRoutes.tsx
@@ -4,9 +4,6 @@ import ProtectedRoute from "./ProtectedRoute";
 import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import HiringManagerDashboardPage from "../pages/HiringManager/HiringManagerDashboardPage";
-import JobPostingsPage from "../pages/JobPostingsPage";
-import JobPostings from "../pages/JobPostingsPage/JobPostings";
-import JobApplication from "../pages/JobPostingsPage/JobApplication";
 
 const AppRoutes = () => {
   return (
@@ -23,11 +20,6 @@ const AppRoutes = () => {
             </ProtectedRoute>
           }
         />
-
-        <Route path="/job-postings" element={<JobPostingsPage />}>
-            <Route index element={<JobPostings />} />
-            <Route path="apply/:id" element={<JobApplication />} />
-        </Route>
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
