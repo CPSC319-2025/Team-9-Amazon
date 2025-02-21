b301c6d90f4cce58fb2b21110830f0053a8676ac
Lizzza <100636075+luevrata@users.noreply.github.com>
2025-02-20 18:59:23 -0800

    apply common styles

--- frontend/src/components/HiringManager/HiringManagerNav.tsx
+++ frontend/src/components/HiringManager/HiringManagerNav.tsx
@@ -2,11 +2,16 @@ import { Box, Typography, Button, Tabs, Tab, Chip } from "@mui/material";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { ROUTES } from "../../routes/routePaths";
+import {
+  chipStyle,
+  colors,
+  filledButtonStyle,
+} from "../../styles/commonStyles";
 
 const HiringManagerNav = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { jobPostingId } = useParams(); 
+  const { jobPostingId } = useParams();
   const [tabValue, setTabValue] = useState(location.pathname);
 
   useEffect(() => {
@@ -19,43 +24,85 @@ const HiringManagerNav = () => {
   };
 
   return (
-    <Box sx={{ backgroundColor: "#fff", boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)", padding: "20px", borderRadius: "8px" }}>
+    <Box
+      sx={{
+        backgroundColor: "#fff",
+        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+        padding: "20px",
+        borderRadius: "8px",
+      }}
+    >
       {/* Job Title & Status */}
-      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 3 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 3,
+        }}
+      >
         <Box>
-          <Typography variant="h4">ML Compiler Software Engineer PEY Co-op</Typography>
-          <Typography variant="body2" color="text.secondary" sx={{ fontSize: "1rem", marginBottom: "8px" }}>
+          <Typography variant="h4">
+            ML Compiler Software Engineer PEY Co-op
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ fontSize: "1rem", marginBottom: "8px" }}
+          >
             (12-16 months), Annapurna ML - Co-op
           </Typography>
-          <Typography variant="body2" sx={{ fontSize: "0.95rem", color: "#333" }}>
-            <strong>Application Received:</strong> 67 | <strong>Machine Evaluated:</strong> 12 | <strong>Processing:</strong> 32
+          <Typography
+            variant="body2"
+            sx={{ fontSize: "0.95rem", color: "#333" }}
+          >
+            <strong>Application Received:</strong> 67 |{" "}
+            <strong>Machine Evaluated:</strong> 12 |{" "}
+            <strong>Processing:</strong> 32
           </Typography>
         </Box>
 
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 0.5 }}>
-        <Chip
-            label="Published"
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <Chip
+            label="PUBLISHED"
             sx={{
-              backgroundColor: "white",
-              color: "#5fa81a",
-              border: "1.5px solid #5fa81a",
-              fontSize: "0.85rem",
-              fontWeight: "500",
-              padding: "6px 12px",
-              borderRadius: "16px"
+              ...chipStyle,
+              color: `${colors.green1}`,
+              borderColor: colors.green1,
             }}
           />
-          {/* <Chip label="Published" sx={{borderColor: "#5fa81a", borderBottomWidth: 3, backgroundColor: "#fff", color: "#5fa81a", fontSize: "0.85rem", fontWeight: "500", padding: "6px 12px", borderRadius: "5px" }} /> */}
-          <Button variant="contained" sx={{ backgroundColor: "#D32F2F", color: "#fff", fontWeight: "500", padding: "8px 16px", borderRadius: "6px", "&:hover": { backgroundColor: "#B71C1C" } }}>
+          <Button
+            variant="contained"
+            sx={{
+              ...filledButtonStyle,
+              backgroundColor: colors.orange1,
+              color: colors.black1,
+            }}
+          >
             CLOSE JOB
           </Button>
         </Box>
       </Box>
 
       {/* Tabs Navigation */}
-      <Tabs value={tabValue} onChange={handleTabChange} centered sx={{ marginTop: 2 }}>
+      <Tabs
+        value={tabValue}
+        onChange={handleTabChange}
+        centered
+        sx={{ marginTop: 2 }}
+      >
         <Tab label="Job Details" value={ROUTES.jobDetails(jobPostingId!)} />
-        <Tab label="Evaluation Metrics" value={ROUTES.evaluationMetrics(jobPostingId!)} />
+        <Tab
+          label="Evaluation Metrics"
+          value={ROUTES.evaluationMetrics(jobPostingId!)}
+        />
         <Tab label="Applications" value={ROUTES.applications(jobPostingId!)} />
         <Tab label="Reports" value={ROUTES.reports(jobPostingId!)} />
       </Tabs>
--- frontend/src/styles/commonStyles.ts
+++ frontend/src/styles/commonStyles.ts
@@ -3,8 +3,11 @@ import { CSSObject } from "@emotion/react";
 export const colors = {
   blue1: "#146eb4",
   gray1: "#f2f2f2",
+  gray2: "#535d69",
   black1: "#232f3e",
   orange1: "#ff9900",
+  green1: "#006727",
+  red1: "#ff0000",
   black: "#000000",
   white: "#ffffff",
 };
@@ -45,6 +48,16 @@ export const filledButtonStyle: CSSObject = {
     opacity: 0.9,
     boxShadow: "none",
   },
+};
+
+export const chipStyle: CSSObject = {
+  backgroundColor: "white",
+  color: colors.gray2,
+  border: `1px solid ${colors.gray2}`,
+  fontWeight: "bold",
+  padding: "6px 12px",
+  borderRadius: "4px",
+  height: "100%",
 };
 
 export const cardStyle: CSSObject = {
