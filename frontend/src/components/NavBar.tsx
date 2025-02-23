c76a1bbd069279fac1ce24fe96e3ebbf1a38616a
Ke Ren <rkglimmer16@gmail.com>
2025-02-23 15:26:23 -0800

    Add routing in top navbar to home page and hiring manager dashboard

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,8 +4,10 @@ import PersonIcon from "@mui/icons-material/Person";
 import GroupIcon from "@mui/icons-material/Group";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
+import WorkIcon from '@mui/icons-material/Work';
 import { useState } from "react";
 import { Link } from "react-router";
+import { ROUTES } from "../routes/routePaths";
 
 const TopNavbar = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -21,7 +23,7 @@ const TopNavbar = () => {
     <AppBar position="sticky" sx={{ backgroundColor: "#fff", color: "#000", padding: "10px" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         {/* AWS Logo */}
-        <Box>
+        <Box component={Link} to="/" sx={{ display: "flex", alignItems: "center" }}>
           <img src="/aws-icon.png" alt="AWS Logo" width={50} />
         </Box>
 
@@ -42,6 +44,13 @@ const TopNavbar = () => {
             </ListItemButton>
             
             <Divider />
+
+            <ListItemButton component={Link} to={ROUTES.hiringManager.hiringManagerDashboard} onClick={handleMenuClose}>
+              <ListItemIcon>
+                <WorkIcon />
+              </ListItemIcon>
+              <ListItemText primary="Hiring Manager Dashboard" />
+            </ListItemButton>
 
             <ListItemButton component={Link} to="/user-management" onClick={handleMenuClose}>
               <ListItemIcon>
