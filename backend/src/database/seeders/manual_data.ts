a5845abeba7f819415eab1beb422171782c5f7e7
Lizzza <100636075+luevrata@users.noreply.github.com>
2025-03-13 22:40:04 -0700

    hash passwords of seeded admins

--- backend/src/database/seeders/manual_data.ts
+++ backend/src/database/seeders/manual_data.ts
@@ -1,37 +1,46 @@
 import { QueryInterface, Sequelize } from "sequelize";
 import Staff, { StaffCreationAttributes } from "../models/staff";
 import { initModels } from "../database";
+import bcrypt from "bcryptjs";
 
-const listOfStaff: StaffCreationAttributes[] = [
-  {
-    email: "violet@CHPostal.com",
-    password: "password",
-    firstName: "Violet",
-    lastName: "Evergarden",
-    isAdmin: true,
-    isHiringManager: true,
-  },
-  {
-    email: "admin@example.com",
-    password: "admin123",
-    firstName: "Admin",
-    lastName: "Staff",
-    isAdmin: true,
-    isHiringManager: true,
-  },
-];
+const saltRounds = 10;
+
+const hashPasswords = async (): Promise<StaffCreationAttributes[]> => {
+  return [
+    {
+      email: "violet@CHPostal.com",
+      password: await bcrypt.hash("password", saltRounds),
+      firstName: "Violet",
+      lastName: "Evergarden",
+      isAdmin: true,
+      isHiringManager: true,
+    },
+    {
+      email: "admin@example.com",
+      password: await bcrypt.hash("admin123", saltRounds),
+      firstName: "Admin",
+      lastName: "Staff",
+      isAdmin: true,
+      isHiringManager: true,
+    },
+  ];
+};
 
 export default {
   up: async (queryInterface: QueryInterface): Promise<void> => {
     initModels(queryInterface.sequelize);
 
-    await Staff.bulkCreate(listOfStaff, {ignoreDuplicates: true});
+    // Ensure passwords are hashed before inserting
+    const listOfStaff = await hashPasswords();
+
+    await Staff.bulkCreate(listOfStaff, { ignoreDuplicates: true });
   },
 
-  down: async (queryInterface: QueryInterface, Sequelize: Sequelize): Promise<void> => {
+  down: async (queryInterface: QueryInterface): Promise<void> => {
     initModels(queryInterface.sequelize);
-    const staffEmails: string[] = listOfStaff.map((staff) => staff.email);
 
-    Staff.destroy({where: {email: staffEmails}});
+    const staffEmails = ["violet@CHPostal.com", "admin@example.com"];
+
+    await Staff.destroy({ where: { email: staffEmails } });
   },
 };
