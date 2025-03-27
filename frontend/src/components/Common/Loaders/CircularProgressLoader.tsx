61d6b5f9a4af84ba0ac3e70216eb239727367ef7
Ke Ren <rkglimmer16@gmail.com>
2025-03-26 18:12:06 -0700

    remove comments

--- frontend/src/components/Common/Loaders/CircularProgressLoader.tsx
+++ frontend/src/components/Common/Loaders/CircularProgressLoader.tsx
@@ -14,9 +14,6 @@ export default function CircularProgressLoader(
   text = "",
   size = 40}: LoadingIndicatorProps) {
   return (
-    // <div className="fixed inset-0 bg-opacity-30 z-50 flex items-center justify-center">
-    //   <CircularProgress size={40} sx={{ color: '#FF9900' }} />
-    // </div>
     <Box
       sx={{
         display: "flex",
