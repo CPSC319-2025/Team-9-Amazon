edf5cadad3ddd65d8a2a87056a7eb8a3813a7f6a
MatthewMyWu <matthew6086888@hotmail.com>
2025-04-12 15:04:51 -0700

    Make job tag list longer

--- frontend/src/pages/JobPostingsPage/JobPostings.tsx
+++ frontend/src/pages/JobPostingsPage/JobPostings.tsx
@@ -154,7 +154,7 @@ export default function JobPostings() {
             minWidth: "250px",
             maxWidth: "250px",
             minHeight: "300px",
-            maxHeight: "400px",
+            height: "100%",
             overflowY: "auto",
           }}
         >
@@ -170,7 +170,7 @@ export default function JobPostings() {
               className="p-2 mb-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
 
-            <div className="flex flex-col gap-2 mb-4 max-h-48 overflow-y-auto">
+            <div className="flex flex-col gap-2 mb-4 max-h-120 overflow-y-auto">
               {jobTypes
                 .filter((type) =>
                   type.toLowerCase().includes(searchTagTerm.toLowerCase())
@@ -214,87 +214,6 @@ export default function JobPostings() {
           )}
         </div>
       </div>
-
-      {/* Job Details Modal */}
-      {/*<Modal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        aria-labelledby="job-modal-title"
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "#f9f9f9",
-            borderRadius: 0,
-            boxShadow: 24,
-            p: 4,
-            maxWidth: "42rem",
-            width: "90%",
-            m: 2,
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            maxHeight: "90vh",
-          }}
-        >
-          {selectedJob && (
-            <>
-              <div className="flex justify-between items-center text-gray-800">
-                <IconButton
-                  onClick={() => setIsModalOpen(false)}
-                  aria-label="close"
-                  size="small"
-                >
-                  <CloseIcon />
-                </IconButton>
-                <h2 className="text-2xl" id="job-modal-title">
-                  {selectedJob.title}
-                </h2>
-                <p>{`#${selectedJob.code}`}</p>
-              </div>
-              <section className="my-2 custom-scrollbar max-h-[calc(90vh-120px)]">
-                <p>
-                  <span className="font-semibold">Location:</span>{" "}
-                  {selectedJob.location}
-                </p>
-                <p>
-                  <span className="font-semibold">Job Type:</span>{" "}
-                  {selectedJob.job_type}
-                </p>
-                <p>
-                  <span className="font-semibold">Posted:</span>{" "}
-                  {selectedJob.posted_at}
-                </p>
-                <div className="mt-4">
-                  <h4 className="font-semibold">About the Role:</h4>
-                  <p>{selectedJob.description}</p>
-                </div>
-                <div className="mt-4">
-                  <h4 className="font-semibold">What You Will Do:</h4>
-                  <ul className="list-disc list-inside pl-4">
-                    {selectedJob.responsibilities.map(
-                      (responsibility, index) => (
-                        <li key={index}>{responsibility}</li>
-                      )
-                    )}
-                  </ul>
-                </div>
-                <div className="mt-4">
-                  <h4 className="font-semibold">What We're Looking For:</h4>
-                  <ul className="list-disc list-inside pl-4">
-                    {selectedJob.qualifications.map((qualification, index) => (
-                      <li key={index}>{qualification}</li>
-                    ))}
-                  </ul>
-                </div>
-              </section>
-            </>
-          )}
-        </Box>
-      </Modal>  */}
     </div>
   );
 }
